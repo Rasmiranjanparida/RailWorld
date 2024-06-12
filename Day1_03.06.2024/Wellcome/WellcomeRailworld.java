1baad88d0aa49ccf51a94baf95fd7909f57669db
rasmi <rasmiranjanparida77@gmail.com>
2024-06-12 23:17:52 +0530

    add Day1

---
+++ Day1_03.06.2024/Wellcome/WellcomeRailworld.java
@@ -0,0 +1,10 @@
+package Wellcome;
+
+public class WellcomeRailworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi i am Rasmi ");
+	}
+
+}
