d0e9dd655ec73138a2148ac68ddf69c98d07e587
rasmi <rasmiranjanparida77@gmail.com>
2024-06-13 08:35:15 +0530

    Day-6

---
+++ Day6_10.06.2024/ArraylistQuestion/Question1.java
@@ -0,0 +1,18 @@
+package ArraylistQuestion;
+
+import java.util.ArrayList;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList< String> list= new ArrayList<>();
+	    list.add("Apple");
+	    list.add("banana");
+	    list.add("Grapes");
+	   
+	    
+
+	}
+
+}
---
+++ Day6_10.06.2024/ArraylistQuestion/Question2.java
@@ -0,0 +1,20 @@
+package ArraylistQuestion;
+
+import java.util.ArrayList;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList< String> list= new ArrayList<>();
+	    list.add("Apple");
+	    list.add("banana");
+	    list.add("Grapes");
+	   System.out.println(list.get(1));
+	   list.set(3, "Dates");
+	   System.out.println(list);
+	   
+
+	}
+
+}
