e9101bc07f854049233e60dfd55477a8090261eb
rasmi <rasmiranjanparida77@gmail.com>
2024-06-13 07:42:31 +0530

    Day-5

---
+++ Day5_07.06.2024/generic/typesafe.java
@@ -0,0 +1,14 @@
+package generic;
+
+import java.util.ArrayList;
+
+public class typesafe {
+	public static void main(String[] args) {
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		arrayList.add(1);
+		arrayList.add(2);
+		System.out.println(arrayList);
+	
+
+	}
+}
